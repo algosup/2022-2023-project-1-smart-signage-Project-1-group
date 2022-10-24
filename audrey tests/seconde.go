5a79b647dbe2846d5e8402111b64ca15cf5340ff
audreytllz <114394252+audreytllz@users.noreply.github.com>
2022-10-24 09:47:47 +0200

    update 2

---
+++ audrey tests/seconde.go
@@ -0,0 +1,20 @@
+package main
+
+var LEDS = [5]bool{true, true, true, false, true}
+
+func Light2(LEDS){
+	for c := 0; c < len(LEDS); c++ {
+		got := true 
+		if got != LEDS[c]{
+			return Error ("une des LEDS est eteinte ")
+			for c := 0; c < len(LEDS); c++ {
+				LEDS[c]:= false
+		}
+	}
+	}
+}
+
+func main() {
+
+	light2()
+}
