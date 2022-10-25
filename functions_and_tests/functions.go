1112bde2e8f142d605ea50dc5f1c45b52c5736c6
audreytllz <114394252+audreytllz@users.noreply.github.com>
2022-10-25 16:40:22 +0200

    updates

--- functions_and_tests/functions.go
+++ functions_and_tests/functions.go
@@ -3,8 +3,11 @@ package function
 type hour struct {
 	hourvar int
 }
+type volt struct {
+	voltage int
+}
 
-func light1(hh int) bool {
+func lightstatus(hh int) bool {
 	h := hour{hh}
 	if h.hourvar >= 6 && h.hourvar <= 1 {
 		return true
@@ -12,61 +15,24 @@ func light1(hh int) bool {
 	return false
 }
 
-// // This function is used to see if a light is not working when it's supposed to be on
+// This function is used to see if a light is not working when it's supposed to be on
 
-// var LEDS = [5]int{11,110,0,10,10}
-func light2(LEDS int) bool {
-	for c := 0; c < len(LEDS); c++ {
-		d := LEDS[c]
-		t:= 10
-		if  t >= d || d <= 0 {
-			return false
-		}
-		return true 
+func lightsvoltage(vv int) bool {
+	v := volt{vv}
+	if v.voltage <= 240 && v.voltage >= 220 {
+		return true
+	}
+	return false
 }
 
-// func light2(LEDS){
-// 	for c := 0; c < len(LEDS); c++ {
-// 		got := true
-// 		if got != LEDS[c]{
-// 			return Error ("A LED is not working")
-// 			for c := 0; c < len(LEDS); c++ {
-// 				LEDS[c]:= false
-// 				return LEDS
-// 		}
-// 		else{
-// 			return LEDS
-// 		}
-// 	}
-// 	}
-// }
+// This function check if lights are on or off
 
-// // This function check if lights are on or off
-
-// var LEDS = [5]bool{true, true, true, true, true}
-// var light int
-
-// func light3(LEDS) {
-// 	if light == "ON" {
-// 		for c := 0; c < len(LEDS); c++ {
-// 			LEDS[c] = false
-// 		}
-// 		light := "OFF"
-// 		return LEDS
-// 	} else if light == "OFF" {
-// 		for c := 0; c < len(LEDS); c++ {
-// 			LEDS[c] = true
-// 		}
-// 		light := "ON"
-// 		return LEDS
-// 	} else {
-// 		return Error("Some LEDS are on and others are off see LEDS state ")
-// 	}
-
-// }
-
-// func main() {
-// 	light1()
-// 	light2()
-// 	light3()
-// }
+func lightsonoff(vv int) int {
+	v := volt{vv}
+	if v.voltage <= 240 && v.voltage >= 220 {
+		return 0
+		// off
+	}
+	return 1
+	// On
+}
--- functions_and_tests/functions_test.go
+++ functions_and_tests/functions_test.go
@@ -4,48 +4,26 @@ import (
 	"testing"
 )
 
-func TestLight1(t *testing.T) {
-	got := light1(3)
+func TestLightstatus(t *testing.T) {
+	got := lightstatus(3)
 	want := false
 	if got != want {
 		t.Error("LED's should be off")
 	}
 }
 
-var LEDS = [5]int{11, 110, 0, 10, 10}
-
-// // TestLight2 is a test function for light2
-func TestLight2(t *testing.T) {
-	got := ligth2(LEDS)
-	want := false
+func TestLightsvoltage(t *testing.T) {
+	got := lightsvoltage(230)
+	want := true
 	if got != want {
-		t.Error("LED's should be off")
+		t.Error(" a LED should be broken ")
 	}
 }
 
-// func TestLight2(t *testing.T) {
-// 	got := rand.Intn(10, 100),
-// 	want := 10
-// 	for c := 0; c < len(LEDS), c++ {
-// 		if got[c]<= wan {
-// 			return Error ("LED's are off")
-// 		}
-// 	}
-// }
-
-// // TestLight3 is a test function for light3
-// func TestLight3(t *testing.T) {
-// 	if  light == 10 {
-// 		return 0
-// 	}else if light == light >10{
-// 		return light "Power of light"
-// 	}else if light < 10 {
-// 		return Error("There is a problem with a LED")
-// 	}
-// }
-
-// func main() {
-// 	TestLight1()
-// 	TestLight2()
-// 	TestLight3()
-// }
+func TestLightsonoff(t *testing.T) {
+	got := lightsonoff(230)
+	want := 0
+	if got != want {
+		t.Error(" a LED should be broken ")
+	}
+}
