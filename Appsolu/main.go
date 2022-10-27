5ffa6e6fd0ea0cd2d991984042039c38b111bd20
Nicolas MIDA <nicolas.mida@algosup.com>
2022-10-27 21:49:13 +0200

    Update main.go
    Finalize the main.go file

--- Appsolu/main.go
+++ Appsolu/main.go
@@ -6,7 +6,7 @@ import (
 	"time"
 )
 
-var uart = machine.UART2
+var uart = machine.UART2 // Define the UART (UART is a serial communication protocol)
 
 // Create a struct to define a new sensor
 type Sensor struct {
@@ -17,35 +17,40 @@ type Led struct {
 	pin machine.Pin
 }
 
-type ADC struct {
+type ADC struct { // ADC is an analog to digital converter
 	pin machine.Pin
 }
 
+// Function to create a new ADC and return it
 func newADC(pin int) *ADC {
 	adc := machine.Pin(pin)
 	adc.Configure(machine.PinConfig{Mode: machine.PinInput})
 	return &ADC{pin: adc}
 }
 
+// Function to get the value of the ADC
 func (adc *ADC) getValue() string {
 	return strconv.FormatBool(adc.pin.Get())
 }
 
+// Function to create a new led and return it
 func newLed(pin int) *Led {
 	led := &Led{pin: machine.Pin(pin)}
 	led.pin.Configure(machine.PinConfig{Mode: machine.PinOutput})
 	return led
 }
 
+// Function to turn on the led
 func (led *Led) On() {
 	led.pin.High()
 }
 
+// Function to turn off the led
 func (led *Led) Off() {
 	led.pin.Low()
 }
 
-// Function to create a new sensor
+// Function to create a new sensor and return it
 func newSensor(pin int) *Sensor {
 	sensor := machine.Pin(pin)
 	sensor.Configure(machine.PinConfig{Mode: machine.PinInput})
@@ -64,36 +69,36 @@ func SendMessage(mess string) {
 	uart.Write([]byte("AT+MSG=" + m + "\r\n"))
 }
 
-// read the pin A12 and send the data to the server
+// Read the pin A12 (we actually getting a bool value) and returning it in a string
 func (sensor *Sensor) getValue() string {
 	return strconv.FormatBool(sensor.pin.Get())
 }
 
 func main() {
 
-	sensor := machine.Pin(machine.PA7)
-	sensor.Configure(machine.PinConfig{Mode: machine.PinInput})
+	sensor := machine.Pin(machine.PA7)                          // Create a new sensor (this one is the photoresistor)
+	sensor.Configure(machine.PinConfig{Mode: machine.PinInput}) // Configure the sensor as an input
 
 	// get the value of the sensor
 	var value = sensor.Get()
 
-	led := machine.Pin(machine.PA12)
-	led.Configure(machine.PinConfig{Mode: machine.PinOutput})
+	led := machine.Pin(machine.PA12)                          // Define the led pin
+	led.Configure(machine.PinConfig{Mode: machine.PinOutput}) // Configure the pin as output
 
-	uart.Configure(machine.UARTConfig{BaudRate: 9600, TX: machine.A2, RX: machine.A3})
+	uart.Configure(machine.UARTConfig{BaudRate: 9600, TX: machine.A2, RX: machine.A3}) // Configure the UART
 
-	WriteATCommand("AT+ JOIN\r\n")
+	WriteATCommand("AT+ JOIN\r\n") // Join the network
 
-	time.Sleep(time.Second * 10)
+	time.Sleep(time.Second * 10) //  We are waiting for the connection to be established
 	SendMessage("Starting the program")
-	time.Sleep(time.Second * 10)
+	time.Sleep(time.Second * 10) // We are waiting for the message to be sent
 
 	for {
 		if value == true { // However we wanted a int value instead of a bool
-			led.Low()
+			led.Low() // Light off the led
 			SendMessage("The sensor said true")
 		} else {
-			led.High()
+			led.High() // Light on the led
 			SendMessage("The sensor said false")
 		}
 	}
